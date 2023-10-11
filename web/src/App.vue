89a16319d1f59049a6635e84b07ea70d909bd136
luvliescrushing <2270635489@qq.com>
2023-10-12 06:47:09 +0800

    init

---
+++ web/src/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+import router from './router';
+</script>
+
+<template>
+  <router-view></router-view>
+</template>
---
+++ web/src/pages/login.vue
@@ -0,0 +1,6 @@
+<template>
+    <el-row style="min-height: 100vh;" >
+        <el-col :span="16" class="bg-indigo-500 flex items-center justify-center flex-col">left</el-col>
+        <el-col :span="8" class="flex items-center justify-center flex-col">right</el-col>
+    </el-row>
+</template>
